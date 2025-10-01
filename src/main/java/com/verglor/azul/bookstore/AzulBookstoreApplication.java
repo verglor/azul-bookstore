17d45411dec01a3c766c8fd0a747ac902359c685
verglor <verglor@verglor.com>
2025-10-01 22:44:48 +0200

    Spring Initializr

---
+++ src/main/java/com/verglor/azul/bookstore/AzulBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.verglor.azul.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzulBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzulBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verglor/azul/bookstore/AzulBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verglor.azul.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzulBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
